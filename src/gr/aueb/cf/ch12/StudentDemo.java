d2482d0c5bd0370aca6fcf435098ab82801bef82
Nick Papadakis <orfneos@yahoo.gr>
2025-01-05 00:38:26 +0200

    updated with overloaded constructor

---
+++ src/gr/aueb/cf/ch12/StudentDemo.java
@@ -0,0 +1,31 @@
+package src.gr.aueb.cf.ch12;
+
+
+public class StudentDemo {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        Student bob = new Student(2, "Bob", "D.");
+
+        bob.setLastname("Dylan");
+
+        student.setId(1);                            //        student.id = 1;
+        student.setFirstname("Alice");                            //        student.firstname = "Alice";
+        student.setLastname("W.");                            //        student.lastname = "W.";
+
+        System.out.println("Id: " + student.getId());
+        System.out.println("Firstname: " + student.getFirstname());
+        System.out.println("Lastname: " + student.getLastname());
+
+        System.out.println("Id: " + bob.getId());
+        System.out.println("Firstname: " + bob.getFirstname());
+        System.out.println("Lastname: " + bob.getLastname());
+
+
+
+
+//        System.out.println("id: " + student.id);
+//        System.out.println("Firstname: " + student.firstname);
+//        System.out.println("Lastname: " + student.lastname);
+    }
+}
