2a2883f35a88cdeb2b02ee423c63223a0fbd8230
Nick Papadakis <orfneos@yahoo.gr>
2025-01-08 21:31:01 +0200

    Order Java Bean Created and main

---
+++ src/gr/aueb/cf/ch13/Account.java
@@ -0,0 +1,163 @@
+package src.gr.aueb.cf.ch13;
+
+/**
+ * Defines a {@link Account} class.
+ * @author nikos
+ * @version 0.1
+ * @since 0.1
+ */
+
+public class Account {
+    private int id;
+    private String iban;
+    private String firstname;
+    private String lastname;
+    private String ssn;
+    private double balance;
+
+    public Account() {
+    }
+
+    public Account(int id, String iban, String firstname, String lastname, String ssn, double balance) {
+        this.id = id;
+        this.iban = iban;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.ssn = ssn;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /*
+     *
+     * Public API
+     */
+
+    /**
+     * Deposit a certain amount of money.
+     * @param amount the amount of money to be deposited.
+     * @throws Exception if the amount is negative
+     */
+    public void deposit(double amount) throws Exception {
+        try {
+            if (amount < 0) {
+                throw new Exception("The amount must not be negative.");
+            }
+            balance += amount;
+            System.out.println("Amount " + amount + "successfully deposited.");   //logging
+        } catch (Exception e) {
+            System.out.println("Error. Amount " + amount + "cannot be negative");
+           // e.printStackTrace();
+            throw e;
+        }
+    }
+
+    /**
+     * Withdraws an amount of money for the {@link Account}
+     * based on a valid ssn checked by {@link #isSsnValid(String)}
+     *
+     * @param amount
+     *          the amount to be withdrawn.
+     * @param ssn
+     *          the given ssn.
+     * @throws Exception
+     *          if the ssn is not valid or the valance is not sufficient or
+     *          the amount is negative.
+     */
+    public void withdraw(double amount, String ssn) throws Exception {
+        try {
+            if (!isSsnValid(ssn)) {
+                throw new Exception("Ssn " + ssn + "is not valid.");
+            }
+
+            if (amount > balance) {
+                throw new Exception("Insufficient balance " + balance + " for amount " + amount);
+            }
+
+            if (amount < 0) {
+                throw new Exception("The amount" + amount + " must not be negative.");
+            }
+
+            balance -= amount;
+            System.out.println("Amount " + amount + "successfully withdrawn.");   //logging
+
+
+        } catch (Exception e) {
+            System.out.println("Error. " + e.getMessage());
+          //  e.printStackTrace();
+            throw e;
+        }
+    }
+
+    /**
+     * Returns the balance of the {@link Account}.
+     *
+     * @return the Account's balance.
+     */
+    public double getAccountBalance() {
+        System.out.println("The account balance was returned"); // logging
+        return getBalance();
+    }
+
+    /**
+     * Returns a string representation of the {@link Account} state.
+     *
+     * @return the string-based format of the state of the {@link  Account}.
+     */
+    public String accountToString() {
+        return "(" + id + ", " + iban + ", " + firstname + ", " + lastname + ", " + ssn + ", " + balance + ")";
+    }
+
+    // internals
+
+    private boolean isSsnValid(String ssn) {
+        return this.ssn.equals(ssn);
+    }
+}
