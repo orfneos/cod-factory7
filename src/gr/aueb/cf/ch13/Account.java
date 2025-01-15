084b29c52652d0a129a6a68e0752b50ff85108d6
Nick Papadakis <orfneos@yahoo.gr>
2025-01-16 01:29:35 +0200

    updates

--- src/gr/aueb/cf/ch13/Account.java
+++ src/gr/aueb/cf/ch13/Account.java
@@ -1,5 +1,9 @@
 package src.gr.aueb.cf.ch13;
 
+import src.gr.aueb.cf.ch13.exceptions.InsufficientBalanceException;
+import src.gr.aueb.cf.ch13.exceptions.NegativeAmountException;
+import src.gr.aueb.cf.ch13.exceptions.SsnNotValidException;
+
 /**
  * Defines a {@link Account} class.
  * @author nikos
@@ -85,14 +89,15 @@ public class Account {
      * @param amount the amount of money to be deposited.
      * @throws Exception if the amount is negative
      */
-    public void deposit(double amount) throws Exception {
+    public void deposit(double amount) throws NegativeAmountException {
         try {
             if (amount < 0) {
-                throw new Exception("The amount must not be negative.");
+//                throw new Exception("The amount must not be negative.");
+                throw new NegativeAmountException(amount);
             }
             balance += amount;
             System.out.println("Amount " + amount + "successfully deposited.");   //logging
-        } catch (Exception e) {
+        } catch (NegativeAmountException e) {
             System.out.println("Error. Amount " + amount + "cannot be negative");
            // e.printStackTrace();
             throw e;
@@ -111,25 +116,28 @@ public class Account {
      *          if the ssn is not valid or the valance is not sufficient or
      *          the amount is negative.
      */
-    public void withdraw(double amount, String ssn) throws Exception {
+    public void withdraw(double amount, String ssn) throws SsnNotValidException, InsufficientBalanceException, NegativeAmountException {
         try {
             if (!isSsnValid(ssn)) {
-                throw new Exception("Ssn " + ssn + "is not valid.");
+//                throw new Exception("Ssn " + ssn + "is not valid.");
+                throw new SsnNotValidException(ssn);
             }
 
             if (amount > balance) {
-                throw new Exception("Insufficient balance " + balance + " for amount " + amount);
+//                throw new Exception("Insufficient balance " + balance + " for amount " + amount);
+                throw new InsufficientBalanceException(balance, amount);
             }
 
             if (amount < 0) {
-                throw new Exception("The amount" + amount + " must not be negative.");
+//                throw new Exception("The amount" + amount + " must not be negative.");
+                throw new NegativeAmountException(amount);
             }
 
             balance -= amount;
             System.out.println("Amount " + amount + "successfully withdrawn.");   //logging
 
 
-        } catch (Exception e) {
+        } catch (SsnNotValidException | InsufficientBalanceException | NegativeAmountException e) {
             System.out.println("Error. " + e.getMessage());
           //  e.printStackTrace();
             throw e;
