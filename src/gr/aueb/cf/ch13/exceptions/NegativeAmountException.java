a2d8f7cdbbcc94df22510cc159e9a82f076891b6
Nick Papadakis <orfneos@yahoo.gr>
2025-01-16 01:12:58 +0200

    updates

---
+++ src/gr/aueb/cf/ch13/exceptions/NegativeAmountException.java
@@ -0,0 +1,7 @@
+package src.gr.aueb.cf.ch13.exceptions;
+
+public class NegativeAmountException extends Exception {
+    public NegativeAmountException(double amount) {
+        super("Amount " + amount + " is sufficient.");
+    }
+}
