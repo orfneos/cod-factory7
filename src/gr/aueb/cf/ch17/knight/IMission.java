31623db27b0ab102cc013fdc15d797b49cd4966b
Nick Papadakis <orfneos@yahoo.gr>
2025-01-25 02:09:55 +0200

    updates

---
+++ src/gr/aueb/cf/ch17/knight/IMission.java
@@ -0,0 +1,7 @@
+package src.gr.aueb.cf.ch17.knight;
+
+@FunctionalInterface
+public interface IMission {
+
+    void embark();
+}
---
+++ src/gr/aueb/cf/ch17/knight/Knight.java
@@ -0,0 +1,8 @@
+package src.gr.aueb.cf.ch17.knight;
+
+public class Knight implements IKnight {
+    @Override
+    public void embarkOnMission(IMission mission) {
+        mission.embark();
+    }
+}
