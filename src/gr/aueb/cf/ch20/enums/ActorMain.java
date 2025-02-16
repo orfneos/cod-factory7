5490da722da4b0f88ee3bb063b980f41b21af117
Nick Papadakis <orfneos@yahoo.gr>
2025-02-16 21:07:24 +0200

    updates

---
+++ src/gr/aueb/cf/ch20/enums/ActorMain.java
@@ -0,0 +1,10 @@
+package src.gr.aueb.cf.ch20.enums;
+
+public class ActorMain {
+
+    public static void main(String[] args) {
+        Actor actor = Actor.STUDENT;
+        System.out.println(actor.toRole());
+        System.out.println(actor.name());
+    }
+}
